c7dc6a86063832b4a6fb1c5c8224c75a16e90ee9
dahoon06 <dahoon226@naver.com>
2025-07-05 20:23:50 +0900

    no message

--- site.config.js
+++ site.config.js
@@ -35,7 +35,7 @@ const CONFIG = {
     },
 
     // CONFIG configration (required)
-    link: "https://dahoon06-store.vercel.app",
+    link: "https://dahoon06.github.io",
     since: 2024, // If leave this empty, current year will be used.
     lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
     ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
