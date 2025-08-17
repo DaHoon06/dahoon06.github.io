ae1d07f9dc460f235abfa35322ed8bc796909371
dahoon06 <dahoon226@naver.com>
2025-08-17 12:53:32 +0900

    #

--- src/widgets/layouts/BaseLayout.tsx
+++ src/widgets/layouts/BaseLayout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode, useEffect, useMemo, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
 import { BaseHeader } from "@widgets/header";
 import { BottomNavigation, Sidebar } from "@widgets/nav";
 
@@ -25,17 +24,11 @@ export const BaseLayout = ({ children, onChangeKeyword }: BaseLayoutProps) => {
             <BaseHeader onChangeKeyword={onChangeKeyword} />
             <div className="xs:p-0 mt-[60px] flex h-[calc(100vh-60px)] w-full pb-[60px]">
                 <Sidebar isActive={isActive} />
-                <AnimatePresence mode="wait">
-                    <motion.main
-                        initial={{ opacity: 0, x: 20 }}
-                        animate={{ opacity: 1, x: 0 }}
-                        exit={{ opacity: 0, x: -20 }}
-                        transition={{ duration: 0.5 }}
+                    <main
                         className="flex-1 overflow-y-auto"
                     >
                         {children}
-                    </motion.main>
-                </AnimatePresence>
+                    </main>
             </div>
             <BottomNavigation
                 className={"xs:hidden z-50 h-[60px]"}
