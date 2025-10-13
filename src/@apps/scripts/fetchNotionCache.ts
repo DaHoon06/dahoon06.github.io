75fdebde19258ba2f6fe9cd77693ed04dedd30b0
dahoon06 <dahoon226@naver.com>
2025-10-14 01:04:23 +0900

    chore: sitemap, robots.txt 추가

--- src/@apps/scripts/fetchNotionCache.ts
+++ src/@apps/scripts/fetchNotionCache.ts
@@ -10,9 +10,37 @@ dotenv.config({ path: ".env" });
 (async () => {
     console.log("Fetching posts from Notion...");
     const posts = await getPosts();
+    const dataDir = path.join(process.cwd(), "public");
+    const sitemapPath = path.join(dataDir, "sitemap.xml");
+
     fs.writeFileSync(
         path.join(process.cwd(), "posts/cachedPosts.json"),
         JSON.stringify(posts, null, 2)
     );
-    console.log("✅ Posts cached to /posts/cachedPosts.json");
+
+    console.log("Posts cached to /posts/cachedPosts.json");
+
+    const baseUrl = "https://dahoon06.github.io";
+
+    const sitemapItems = posts
+        .map((post) => {
+            const url = `${baseUrl}/posts/${post.slug}`;
+            return `
+        <url>
+          <loc>${url}</loc>
+          <lastmod>${new Date(post.createdTime).toISOString()}</lastmod>
+          <changefreq>weekly</changefreq>
+          <priority>0.8</priority>
+        </url>
+      `;
+        })
+        .join("\n");
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${sitemapItems}
+  </urlset>`;
+
+    fs.writeFileSync(sitemapPath, sitemap);
+    console.log(`Generated sitemap.xml → ${sitemapPath}`);
 })();
