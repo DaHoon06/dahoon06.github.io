e39a13ed9253da0af4c29d5278fd28fb836f0e07
dahoon06 <dahoon226@naver.com>
2025-10-15 00:43:46 +0900

    chore: Add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,23 @@ import MetaHead from "@shared/ui/heads/MetaHead";
 import { HydrationBoundary, QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@shared/libs/react-query";
 import { RouteProgressBar } from "@shared/ui/progress-bar";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-F3D23SCNJB"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-F3D23SCNJB');
+        `}
+            </Script>
             <QueryClientProvider client={queryClient}>
                 <HydrationBoundary state={pageProps.dehydratedState}>
                     <RouteProgressBar />
